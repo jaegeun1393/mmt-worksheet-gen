9e404c31e3f165dae7681877ba3b03e85ff4ca39
jaegeun1393 <46430624+jaegeun1393@users.noreply.github.com>
2021-08-07 20:22:17 -0700

    Add files via upload

--- app/static/js/userinfo.js
+++ app/static/js/userinfo.js
@@ -4,10 +4,13 @@
 
 function separate() {
 	let value = document.getElementById("data").innerHTML;
-	console.log(value);
 	let myArr = value.split(',');
 	myArr[1] = myArr[1]. slice(2, -1);
-	myArr[4] = myArr[4]. slice(2, -2);
-	let final = "Welcome back " + myArr[1] + " " + myArr[4] + "!"
+	myArr[6] = myArr[6]. slice(2, -2);
+	let final = "Welcome " + myArr[1] + " " + myArr[6] + "!"
 	document.getElementById("username").innerHTML =  final;
+}
+
+function studentopt() {
+
 }
